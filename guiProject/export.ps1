58fdebfc14700714c988c9fe0eac65f5a9a95ec6
Laura-Cahill <lcahill416@gmail.com>
2025-08-14 17:13:55 -0400

    Delete guiProject/export.ps1

--- guiProject/export.ps1
+++
@@ -1,9 +0,0 @@
-# Run in WSL
-GOOS=linux go build .
-
-# Run in Powershell
-ssh 141.148.50.200 "sudo systemctl stop guiproject"
-ssh 141.148.50.200 "rm -rf /home/ubuntu/guiProject"
-scp -r "\\wsl.localhost\Ubuntu\home\lcahill\guiProject" 141.148.50.200:/home/ubuntu/
-ssh 141.148.50.200 "chmod +x /home/ubuntu/guiProject/guiProject"
-ssh 141.148.50.200 "sudo systemctl start guiproject"
